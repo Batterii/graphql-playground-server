3b3e272b4a89dc50b67577bcb1f5b07d21536e35
Steve Ripberger <sripberger@batterii.com>
2020-05-11 21:07:52 -0400

    Add support for --version flag

--- graphql-playground-server.js
+++ graphql-playground-server.js
@@ -1,6 +1,13 @@
 #! /usr/bin/env node
 const Koa = require('koa');
 const { default: playground } = require('graphql-playground-middleware-koa');
+const pkg = require('./package.json');
+
+const arg = process.argv[2];
+if (arg === '--version' || arg === '-v') {
+	console.log(pkg.version);
+	process.exit(0);
+}
 
 const port = process.env.GRAPHQL_PLAYGROUND_PORT || 3001
 const endpoint = process.env.GRAPHQL_PLAYGROUND_ENDPOINT ||
